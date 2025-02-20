e5cce747d78de560f5727858a8dc5b9cc00876b5
torbenconto <torbenmconto@gmail.com>
2025-02-20 08:43:17 -0500

    Use a header struct instead of writing raw bytes for pkt header.

--- pkt/pkt.go
+++ pkt/pkt.go
@@ -12,24 +12,52 @@ import (
 )
 
 const (
-	MagicNumber      uint32 = 0x504B5400
-	Version          byte   = 0x01
-	LittleEndianFlag byte   = 0x00
-	BigEndianFlag    byte   = 0x01
-	HeaderLength     int    = 12
+	MagicNumber uint32 = 0x504B5400
+	Version     byte   = 0x01
 )
+
+const HeaderLength int = 12
+
+const (
+	LittleEndianFlag byte = 0x00
+	BigEndianFlag    byte = 0x01
+)
+
+type Header struct {
+	MagicNumber uint32
+	Version     byte
+	Endian      byte
+	Length      uint32
+}
+
+func (h Header) Bytes() []byte {
+	header := make([]byte, HeaderLength)
+	binary.BigEndian.PutUint32(header[0:4], h.MagicNumber)
+
+	header[4] = h.Version
+	header[5] = h.Endian
+
+	// Reserved
+	binary.BigEndian.PutUint32(header[6:8], 0)
+
+	// Packet length
+	binary.BigEndian.PutUint32(header[8:12], h.Length)
+
+	return header
+}
 
 // Write writes packets to a .pkt file.
 func Write(w io.Writer, packets []_packets.Packet) error {
 	// Write the header (big-endian)
-	header := make([]byte, HeaderLength)
-	binary.BigEndian.PutUint32(header[0:4], MagicNumber)
-	header[4] = Version
-	header[5] = BigEndianFlag                  // Big-endian
-	binary.BigEndian.PutUint16(header[6:8], 0) // Reserved
-	binary.BigEndian.PutUint32(header[8:12], uint32(len(packets)))
 
-	if _, err := w.Write(header); err != nil {
+	header := Header{
+		MagicNumber: MagicNumber,
+		Version:     Version,
+		Endian:      LittleEndianFlag,
+		Length:      uint32(len(packets)),
+	}
+
+	if _, err := w.Write(header.Bytes()); err != nil {
 		return err
 	}
 
@@ -76,22 +104,39 @@ func Write(w io.Writer, packets []_packets.Packet) error {
 
 // Read reads packets from a .pkt file.
 func Read(r io.Reader) ([]_packets.Packet, error) {
-	// Read and validate the header
-	header := make([]byte, HeaderLength)
-	if _, err := r.Read(header); err != nil {
+	var header Header
+
+	// Read and decode the header
+	if err := binary.Read(r, binary.LittleEndian, &header.MagicNumber); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.Version); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.Endian); err != nil {
 		return nil, err
 	}
 
-	if binary.BigEndian.Uint32(header[0:4]) != MagicNumber {
+	// Skip reserved bytes
+	var reserved uint16
+	if err := binary.Read(r, binary.BigEndian, &reserved); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Read(r, binary.LittleEndian, &header.Length); err != nil {
+		return nil, err
+	}
+
+	// Validate header
+	if header.MagicNumber != MagicNumber {
 		return nil, fmt.Errorf("invalid magic number")
 	}
-
-	if header[5] != BigEndianFlag { // Expecting big-endian
-		return nil, fmt.Errorf("unsupported endianness (expected big-endian)")
+	if header.Endian != LittleEndianFlag {
+		return nil, fmt.Errorf("unsupported endianness (expected little-endian)")
 	}
 
-	packetCount := binary.BigEndian.Uint32(header[8:12])
-	var packets []_packets.Packet
+	packetCount := header.Length
+	packets := make([]_packets.Packet, 0, packetCount)
 
 	for i := uint32(0); i < packetCount; i++ {
 		var packetLen uint32
@@ -106,10 +151,8 @@ func Read(r io.Reader) ([]_packets.Packet, error) {
 		switch packetType {
 		case _packets.ARPCODE:
 			packet = arp.NewPacket()
-		}
-
-		if packet == nil {
-			return nil, fmt.Errorf("nil packet encountered")
+		default:
+			return nil, fmt.Errorf("unknown packet type: %d", packetType)
 		}
 
 		// Read timestamp
@@ -127,7 +170,7 @@ func Read(r io.Reader) ([]_packets.Packet, error) {
 
 		// Read packet data
 		packetBytes := make([]byte, packetLen)
-		if _, err := r.Read(packetBytes); err != nil {
+		if _, err := io.ReadFull(r, packetBytes); err != nil {
 			return nil, err
 		}
 
@@ -136,7 +179,6 @@ func Read(r io.Reader) ([]_packets.Packet, error) {
 			return nil, err
 		}
 
-		// Append to packet slice
 		packets = append(packets, packet)
 	}
 
